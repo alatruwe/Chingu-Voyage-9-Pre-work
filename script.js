9b6a4f300ae0942cb9d632139327c8de3484bd4b
adelinel <adeline.latruwe@gmail.com>
2019-05-25 21:35:44 -0700

    main content - reddit news feed implemented

---
+++ script.js
@@ -0,0 +1,30 @@
+
+/*JSON Array
+"mockup" :[
+    {"content": "Posted by:"},
+    {"content":"Javascript's for loop is surprisingly fast", "user":"TurnaroundGames"}, 
+    {"content":"great article to know about async programming in node js", "user":"h_sa1994"}, 
+    {"content":"Newbie question, mis-typing variable names", "user":"blitz4"},
+    {"content":"I made a version of Gitfolio with vanilla JS", "user":"weakN0"},
+    {"content":"Serverless (cloud synced) storage solutions?", "user":"MrXyfir"},
+    {"content":"Async/Await Issue? Scope Issue?", "user":"Jeffylew77"},
+    {"content":"Feeling the imposter syndrome hard, need advice", "user":"imanuglyperson"},
+    {"content":"Regex and Unicode in javascript", "user":"brn27"},
+    {"content":"Github - dsa.js: Data Structures and Algorithms explained and implemented in Javascript", "user":"adriansky"},
+    {"content":"NPM package for javascript/Node.js competitive programmers", "user":"themanish003"},
+  ]
+*/
+
+var text = '{ "mockup" : [' +
+'{ "content":"Javascript for loop is surprisingly fast" , "user":"TurnaroundGames" },' +
+'{ "content":"great article to know about async programming in node js" , "user":"h_sa1994" },' +
+'{ "content":"Newbie question, mis-typing variable names" , "user":"blitz4" },' +
+'{ "content":"I made a version of Gitfolio with vanilla JS" , "user":"weakN0" },' +
+'{ "content":"Serverless (cloud synced) storage solutions?" , "user":"MrXyfir" },' +
+'{ "content":"Async/Await Issue? Scope Issue?" , "user":"Jeffylew77" },' +
+'{ "content":"Feeling the imposter syndrome hard, need advice" , "user":"imanuglyperson" },' +
+'{ "content":"Regex and Unicode in javascript" , "user":"brn27" },' +
+'{ "content":"Github - dsa.js: Data Structures and Algorithms explained and implemented in Javascript" , "user":"adriansky" },' +
+'{ "content":"NPM package for javascript/Node.js competitive programmers" , "user":"themanish003" } ]}';
+
+var obj = JSON.parse(text);
