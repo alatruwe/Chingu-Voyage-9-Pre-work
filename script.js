3b2e2af85d04113aca9d05c65b1c7d138ffd5205
adelinel <adeline.latruwe@gmail.com>
2019-05-26 20:53:05 -0700

    added time  js clock component

--- script.js
+++ script.js
@@ -28,3 +28,45 @@ var text = '{ "mockup" : [' +
 '{ "content":"NPM package for javascript/Node.js competitive programmers" , "user":"themanish003" } ]}';
 
 var obj = JSON.parse(text);
+
+
+var text2 = '{ "mockup2" : [' +
+'{ "history":"Basic Javascript | freeCodeCamp" , "time":"5 minutes ago" },' +
+'{ "history":"Javascript Function Invocation" , "time":"42 minutes ago" },' +
+'{ "history":"Javascript Output" , "time":"8 hours ago" },' +
+'{ "history":"Javascript Syntax" , "time":"8 hours ago" },' +
+'{ "history":"Javascript Variables" , "time":"8 hours ago" },' +
+'{ "history":"Javascript Data Types" , "time":"8 hours ago" },' +
+'{ "history":"Javascript Functions" , "time":"8 hours ago" },' +
+'{ "history":"Javascript Objects" , "time":"8 hours ago" },' +
+'{ "history":"Javascript Regular Expressions" , "time":"8 hours ago" },' +
+'{ "history":"Javascript Debugging" , "time":"8 hours ago" },' +
+'{ "history":"Javascript Coding Conventions" , "time":"8 hours ago" },' +
+'{ "history":"Javascript Best Practices" , "time":"8 hours ago" } ]}';
+
+var obj2 = JSON.parse(text2);
+
+
+
+var dateInfo = new Date();
+
+/* time */
+var hr,
+  _min = (dateInfo.getMinutes() < 10) ? "0" + dateInfo.getMinutes() : dateInfo.getMinutes(),
+  ampm = (dateInfo.getHours() >= 12) ? "PM" : "AM";
+
+// replace 0 with 12 at midnight, subtract 12 from hour if 13–23
+if (dateInfo.getHours() == 0) {
+  hr = 12;
+} else if (dateInfo.getHours() > 12) {
+  hr = dateInfo.getHours() - 12;
+} else {
+  hr = dateInfo.getHours();
+}
+
+var currentTime = hr + ":" + _min;
+
+// print time
+document.getElementsByClassName("hm")[0].innerHTML = currentTime;
+document.getElementsByClassName("ampm")[0].innerHTML = ampm;
+
